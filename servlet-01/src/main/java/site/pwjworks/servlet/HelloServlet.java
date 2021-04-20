33666889221c0612537bf2f5727aefdda5f77263
pwjworks <13610513466@163.com>
2021-04-20 12:36:15 +0800

    HelloServlet

---
+++ servlet-01/src/main/java/site/pwjworks/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package site.pwjworks.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("进入doget");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello,Servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
